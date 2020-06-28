4d443ad969f50fd31c5b406962a9e17643a4451e
Maxim Volgin <maxvol@users.noreply.github.com>
2020-06-28 19:31:55 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-  
 // swift-tools-version:5.1
+
 import PackageDescription
 
 let package = Package(
     name: "RxVision",
     platforms: [
-        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v3)
+        .macOS(.v10_10), .iOS(.v8), // .tvOS(.v9), .watchOS(.v3)
     ],
     products: [
         .library(
