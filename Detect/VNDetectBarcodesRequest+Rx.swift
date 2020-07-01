f40e88e511fbc866f492d566c47e5e5be8523bfa
Maxim Volgin <maxvol@users.noreply.github.com>
2020-07-01 06:33:41 +0200

    Update VNDetectBarcodesRequest+Rx.swift

--- Detect/VNDetectBarcodesRequest+Rx.swift
+++ Detect/VNDetectBarcodesRequest+Rx.swift
@@ -9,6 +9,7 @@
 import Vision
 import RxSwift
 
+@available(iOS 11.0, *)
 extension Reactive where Base: VNDetectBarcodesRequest {
     
     public static func request<T>(symbologies: [VNBarcodeSymbology]) -> RxVNDetectBarcodesRequest<T> {
