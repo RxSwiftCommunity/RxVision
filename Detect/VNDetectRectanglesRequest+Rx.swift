8858c34b62a8321b9bcb316993ef2652bd433485
Maxim Volgin <maxvol@users.noreply.github.com>
2020-07-01 06:34:50 +0200

    Update VNDetectRectanglesRequest+Rx.swift

--- Detect/VNDetectRectanglesRequest+Rx.swift
+++ Detect/VNDetectRectanglesRequest+Rx.swift
@@ -9,6 +9,7 @@
 import Vision
 import RxSwift
 
+@available(iOS 11.0, *)
 extension Reactive where Base: VNDetectRectanglesRequest {
     
     public static func request<T>() -> RxVNDetectRectanglesRequest<T> {
